696139a7fde6f4f28a401c4376ff7bb98f71e2a3
fdumbioinfo <florent.dumont.bioinfo@gmail.com>
2025-12-11 22:50:42 +0100

    update moal demo moal require

--- moal-demo/7-moal-normalization-demo.r
+++ moal-demo/7-moal-normalization-demo.r
@@ -6,6 +6,7 @@
 # -----
 #
 # loading libraries
+if(!require("moal",quietly=TRUE)){source("https://raw.githubusercontent.com/fdumbioinfo/rtools/main/moal-demo/0-moal-install-r-universe.r")}
 if(!require("data.table",quietly=TRUE)){install.packages("data.table")}
 library(moal);moal::env()
 ??moal::norm
