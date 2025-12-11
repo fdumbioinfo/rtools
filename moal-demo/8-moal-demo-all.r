b791aa5ef05b6dc4d6bccd51306f30f03ef46b13
fdumbioinfo <florent.dumont.bioinfo@gmail.com>
2025-12-11 21:59:20 +0100

    Update 8-moal-demo-all.r

--- moal-demo/8-moal-demo-all.r
+++ moal-demo/8-moal-demo-all.r
@@ -7,20 +7,20 @@
 #
 # change working directory
 # setwd("~/Desktop/rwd/moal-demo")
-# 1 - moal install
-source("https://raw.githubusercontent.com/fdumbioinfo/rtools/main/moal-demo/1-moal-install-r-universe.r")
-# 2 - omic analysis
-source("https://raw.githubusercontent.com/fdumbioinfo/rtools/main/moal-demo/2-moal-omic-demo.r")
-# 3 - functionnal analysis
-source("https://raw.githubusercontent.com/fdumbioinfo/rtools/main/moal-demo/3-moal-ena-demo.r")
-# 4 - volcanoplot
-source("https://raw.githubusercontent.com/fdumbioinfo/rtools/main/moal-demo/4-moal-volcanoplot-demo.r")
-# 5 - QCs
-source("https://raw.githubusercontent.com/fdumbioinfo/rtools/main/moal-demo/5-moal-QCs-demo.r")
-# 6 - heatmap
-source("https://raw.githubusercontent.com/fdumbioinfo/rtools/main/moal-demo/6-moal-heatmap-demo.r")
-# 7 - venn
-source("https://raw.githubusercontent.com/fdumbioinfo/rtools/main/moal-demo/7-moal-venn-demo.r")
-# 8 - normalization
-source("https://raw.githubusercontent.com/fdumbioinfo/rtools/main/moal-demo/8-moal-normalization-demo.r")
+# 0 - moal install
+source("https://raw.githubusercontent.com/fdumbioinfo/rtools/main/moal-demo/0-moal-install-r-universe.r")
+# 1 - omic analysis
+source("https://raw.githubusercontent.com/fdumbioinfo/rtools/main/moal-demo/1-moal-omic-demo.r")
+# 2 - functionnal analysis
+source("https://raw.githubusercontent.com/fdumbioinfo/rtools/main/moal-demo/2-moal-ena-demo.r")
+# 3 - volcanoplot
+source("https://raw.githubusercontent.com/fdumbioinfo/rtools/main/moal-demo/3-moal-volcanoplot-demo.r")
+# 4 - QCs
+source("https://raw.githubusercontent.com/fdumbioinfo/rtools/main/moal-demo/4-moal-QCs-demo.r")
+# 5 - heatmap
+source("https://raw.githubusercontent.com/fdumbioinfo/rtools/main/moal-demo/5-moal-heatmap-demo.r")
+# 6 - venn
+source("https://raw.githubusercontent.com/fdumbioinfo/rtools/main/moal-demo/6-moal-venn-demo.r")
+# 7 - normalization
+source("https://raw.githubusercontent.com/fdumbioinfo/rtools/main/moal-demo/7-moal-normalization-demo.r")
 #
